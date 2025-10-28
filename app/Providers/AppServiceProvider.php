67a08acd7bf1840872ff63d1ed41e1a8f6ac13e1
Mohya972 <nfloh.noel@gmail.com>
2025-10-28 09:54:18 +0100

    Création de la route, authcontroller,

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Vérifier que l'utilisateur est authentifié
+        Gate::define('access-auth', function (User $user) {
+            return $user->is_auth==true;
+        });
     }
 }
