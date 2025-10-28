144b10a44b4fc14cfc40e4119c7aa68ef581d7d6
Mohya972 <nfloh.noel@gmail.com>
2025-10-28 12:32:57 +0100

    Amorces du Controller et de routes

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    // Affiche la liste des taches
+    public function index()
+    {
+        // Récupérer la liste des taches
+        $tasks = Task::where('user_id', auth()->id)->get();
+        
+        return view('index', compact('tasks'));
+    }
+
+    // affiche le formulaire de création de tache
+    public function create()
+    {
+        //
+        return view('tasks.create');
+        
+    }
+
+    // Enregistre une nouvelle tache avec l'id de l'utilisateur connecté
+    public function store(Request $request)
+    {
+        // Validation des champs du formulaire
+        $validated = $request->validate([
+            'user_id'=>'required'
+        ]);
+
+        // Pour éviter tout message d'erreur due à l'absence du 'state' 
+        $validated['state'] = 0;
+
+        // Enregistrement en base
+        Task::create($validated);
+
+        return redirect()->route('task.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    //public function show(string $id) {}
+
+    // Affiche le formulaire de modification de tache
+    public function edit(string $id)
+    {
+        // Récupérer la tache de l'utilisateur
+        $task = Task::findOrFail($id);
+
+        // permet de modifier une tache si elle appartient à l'utilisateur
+        if ($task->user_id == auth()->id) { 
+            return view('tasks.edit');
+        } else {
+            return redirect()->route('tasks.edit');
+        }
+        
+    }
+
+    // Met à jour la tache d'un utilisateur
+    public function update(Request $request, string $id)
+    {
+        // Récupérer la tache de l'utilisateur
+        $task = Task::findOrFail($id);
+
+        // Changer l'état d'une tache si elle appartient à l'utilisateur
+        if ($task->user_id == auth()->id) { 
+            $task->update(['state' => 1]);
+        }
+        
+        // Redirection
+        return redirect()->route('task.index');
+    }
+
+    // Supprime la tache d'un utilisateur
+    public function destroy(string $id)
+    {
+        // Récupérer la tache de l'utilisateur
+        $task = Task::findOrFail($id);
+
+        // Requête pour supprimer la tache si elle appartient à l'utilisateur
+        if ($task->user_id == auth()->id) { 
+            $task->delete();
+        }
+        
+        // Redirection
+        return redirect()->route('task.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
@@ -14,6 +15,12 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/auth', [AuthController::class, 'index'])->name('auth.index');
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/tasks', [TaskController::class, 'create'])->name('tasks.create');
+    Route::get('/tasks/{id}', [TaskController::class, 'edit'])->name('tasks.edit'); //affiche le formulaire de modification à partir de l'id
+    Route::post('/tasks/{id}', [TaskController::class, 'store'])->name('tasks.store'); // enregistre à partir de l'id
+    Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update'); // met à jour à partir de l'id 
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy'); // supprime à partir de l'id
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
